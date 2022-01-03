22099d92f8afbed73b40103bb3fc048b06c88cd1
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2022-01-04 02:21:05 +0300

    `GetListOfAccessibleGitSubmodulesAsyncProviderNS` Added retry logic to `GetListOfAccessibleGitSubmodulesAsync`

--- GetListOfAccessibleGitSubmodulesAsyncProviderNS/GetListOfAccessibleGitSubmodulesAsyncProvider.cs
+++ GetListOfAccessibleGitSubmodulesAsyncProviderNS/GetListOfAccessibleGitSubmodulesAsyncProvider.cs
@@ -11,6 +11,11 @@ namespace GetListOfAccessibleGitSubmodulesAsyncProviderNS;
 /// </summary>
 public static class GetListOfAccessibleGitSubmodulesAsyncProvider
 {
+    /// <summary>
+    ///     Sometimes git provider doesn't answer, hence we need to retry some times
+    /// </summary>
+    private const uint RetryCount = 3;
+
     public static async Task<GitSubmoduleInfo[]> GetListOfAccessibleGitSubmodulesAsync(
         IEnumerable<GitSubmoduleInfo> gitSubmoduleInfoList,
         CancellationToken cancellationToken
@@ -20,15 +25,31 @@ public static class GetListOfAccessibleGitSubmodulesAsyncProvider
         var gitSubmoduleInfoAndIsAccessibleEnumerable = gitSubmoduleInfoList
             .Select(selector: async gitSubmoduleInfo =>
             {
-                var isAccessibleCliExitCode = await ExecuteCliCommandAsyncProvider.ExecuteCliCommandAsync(
-                    cliCommandText: $"git ls-remote \"{gitSubmoduleInfo.Url}\"",
-                    console: systemConsole,
-                    cancellationToken: cancellationToken
-                );
+                var isSuccessfulCliExitCode = false;
+
+                var retriesLeft = RetryCount;
+                while (retriesLeft > 0)
+                {
+                    isSuccessfulCliExitCode =(
+                        await ExecuteCliCommandAsyncProvider.ExecuteCliCommandAsync(
+                            cliCommandText: $"git ls-remote \"{gitSubmoduleInfo.Url}\"",
+                            console: systemConsole,
+                            cancellationToken: cancellationToken
+                        )
+                    ).IsSuccessfulCliExitCode();
+
+                    if (isSuccessfulCliExitCode)
+                    {
+                        break;
+                    }
+
+                    --retriesLeft;
+                }
+
                 return new
                 {
                     GitSubmoduleInfo = gitSubmoduleInfo,
-                    IsAccessible = isAccessibleCliExitCode.IsSuccessfulCliExitCode()
+                    IsAccessible = isSuccessfulCliExitCode
                 };
             });
 
